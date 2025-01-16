a05236e13468724f8ebfd2d185eaa7e630c2e33d
emmanuelojiji <emmanuelojiji@hotmail.com>
2025-01-16 05:05:30 +0000

    refactor: margin left right added to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,15 @@ import logo from "../media/eua-logo.svg";
 const Header = () => {
   return (
     <header>
-      <img src={logo} />
-      <nav>Manufacturer</nav>
-      <nav>Knowledge Hub</nav>
-      <nav>About us</nav>
-      <nav>Contact us</nav>
+      <div className="header-content">
+        <img src={logo} />
+        <nav>Manufacturer</nav>
+        <nav>Knowledge Hub</nav>
+        <nav>About us</nav>
+        <nav>Contact us</nav>
 
-      <p>+44 1785 303300</p>
+        <p>+44 1785 303300</p>
+      </div>
     </header>
   );
 };
