14e11d589a5fd5cf409ebc45a94176f11955e713
emmanuelojiji <emmanuelojiji@hotmail.com>
2025-01-16 13:48:36 +0000

    feat: part box animation working

--- src/App.js
+++ src/App.js
@@ -3,53 +3,9 @@ import "./App.scss";
 import Header from "./components/Header";
 import globe from "./media/globe.webp";
 import { useEffect, useState, useRef } from "react";
+import ValueProposition from "./components/ValueProposition";
 
 function App() {
-  const [scrollY, setScrollY] = useState(0);
-  const [display, setDisplay] = useState("none");
-  const [animationState, setAnimationState] = useState("");
-  const [imageState, setImageState] = useState("");
-  const [hasShrunk, setHasShrunk] = useState(false);
-
-  const displayTimeoutRef = useRef(null);
-
-  const handleScroll = () => {
-    const currentScrollY = window.scrollY;
-    setScrollY(currentScrollY);
-
-    if (currentScrollY >= 200 && !hasShrunk) {
-      if (displayTimeoutRef.current) clearTimeout(displayTimeoutRef.current);
-      setHasShrunk(true);
-      setImageState("shrink");
-      setAnimationState("showText");
-
-      setTimeout(() => {
-        setDisplay("flex");
-      }, 300);
-    } else if (currentScrollY < 200 && hasShrunk) {
-      if (displayTimeoutRef.current) clearTimeout(displayTimeoutRef.current);
-      setHasShrunk(false);
-      setAnimationState("hideText");
-
-      setTimeout(() => {
-        setImageState("grow");
-      }, 300);
-
-      setTimeout(() => {
-        setDisplay("none");
-      }, 500);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-      if (displayTimeoutRef.current) clearTimeout(displayTimeoutRef.current);
-    };
-  }, [hasShrunk]);
-
   return (
     <div className="App">
       <div className="app-wrap">
@@ -67,27 +23,7 @@ function App() {
           </div>
         </section>
 
-        <section className="value-proposition-container">
-          <img src={globe} className={imageState} alt="Globe" />
-          <div
-            className={`value-proposition-text-container ${animationState}`}
-            style={{ display: display }}
-          >
-            <h2>
-              Value Proposition -{" "}
-              <span className="grey">
-                Speak to the customer's wants and needs.
-              </span>
-            </h2>
-            <p>
-              Speak to the customer’s wants and needs. Outline how our service
-              can be the answer to their problems. Help them to make
-              manufacturing possible. Position the human element through the
-              lens of their eyes.
-            </p>
-            <button>CTA</button>
-          </div>
-        </section>
+        <ValueProposition />
       </div>
     </div>
   );
---
+++ src/components/ValueProposition.js
@@ -0,0 +1,98 @@
+import { useEffect, useRef, useState } from "react";
+import "./ValueProposition.scss";
+import globe from "../media/globe.webp";
+
+const ValueProposition = () => {
+  const [scrollY, setScrollY] = useState(0);
+  const [textDisplay, setTextDisplay] = useState("none");
+  const [textAnimationState, setTextAnimationState] = useState("");
+  const [imageState, setImageState] = useState("");
+  const [hasShrunk, setHasShrunk] = useState(false);
+
+  const [boxDisplay, setBoxDisplay] = useState("none");
+  const [boxAnimationState, setBoxAnimationState] = useState("");
+
+  const displayTimeoutRef = useRef(null);
+
+  const handleScroll = () => {
+    const currentScrollY = window.scrollY;
+    setScrollY(currentScrollY);
+
+    if (currentScrollY >= 200 && !hasShrunk) {
+      if (displayTimeoutRef.current) clearTimeout(displayTimeoutRef.current);
+      setHasShrunk(true);
+      setImageState("shrink");
+      setTextAnimationState("showText");
+
+      setTimeout(() => {
+        setBoxDisplay("block");
+      }, 500);
+
+      setBoxAnimationState("showBox");
+
+      setTimeout(() => {
+        setTextDisplay("flex");
+      }, 300);
+    } else if (currentScrollY < 200 && hasShrunk) {
+      if (displayTimeoutRef.current) clearTimeout(displayTimeoutRef.current);
+      setHasShrunk(false);
+
+      setBoxAnimationState("hideBox");
+
+      setTimeout(() => {
+        setBoxDisplay("none");
+      }, 300);
+
+      setTextAnimationState("hideText");
+
+      setTimeout(() => {
+        setImageState("grow");
+      }, 300);
+
+      setTimeout(() => {
+        setTextDisplay("none");
+      }, 500);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      if (displayTimeoutRef.current) clearTimeout(displayTimeoutRef.current);
+    };
+  }, [hasShrunk]);
+
+  return (
+    <section className="value-proposition-container">
+      <div className={`globe-container ${imageState}`}>
+        <div
+          className={`parts-box-container ${boxAnimationState}`}
+          style={{ display: boxDisplay }}
+        >
+          <div className="parts-box-nop">3,045,928 parts</div>
+          <p>Lorem ipsum dolor sit amet, consectetur</p>
+        </div>
+        <img src={globe} alt="Globe" />
+      </div>
+      <div
+        className={`value-proposition-text-container ${textAnimationState}`}
+        style={{ display: textDisplay }}
+      >
+        <h2>
+          Value Proposition -{" "}
+          <span className="grey">Speak to the customer's wants and needs.</span>
+        </h2>
+        <p>
+          Speak to the customer’s wants and needs. Outline how our service can
+          be the answer to their problems. Help them to make manufacturing
+          possible. Position the human element through the lens of their eyes.
+        </p>
+        <button>CTA</button>
+      </div>
+    </section>
+  );
+};
+
+export default ValueProposition;
